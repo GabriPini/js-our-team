3fb95529fbe5d1be8c06aeb800745489b871dbce
GabriPini <lele19977@gmail.com>
2022-04-07 16:56:23 +0200

    add a new bonus to add a member

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@
 const memeberOnPage =  document.querySelector('.members')
 const addButton = document.querySelector('.btn')
 
-
 const team = [
     {
       name: 'Wayne Barnett',
@@ -66,30 +65,29 @@ console.log (team)
 
 
 //prove per aggiugere qualcuno(still working on it )
-/* addButton.addEventListener('click', function () {
+addButton.addEventListener('click', function () {
 
     team.push({ //Dare la possibilità all’utente di aggiungere un membro 
 
         name : document.getElementById('name').value ,
         role :  document.getElementById('role').value ,
-        image :  document.getElementById('img').value 
+/*         image :  document.getElementById('img').value , */ 
     
     })
-    for (let i  = 0 ; i < team.length ; i++) {
     const memberpage = document.createElement(`div`);
-    const member = team[i];
+    let addmember = team[team.length - 1];
     memberpage.innerHTML =
     ` 
-    <img src="assets/img/${ member.image}" alt="">
+    <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f80b6e3-05fc-4af2-a638-a32ce20652db/d2uixi9-0df6dc1d-8373-419a-a592-dc82d628d86f.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmODBiNmUzLTA1ZmMtNGFmMi1hNjM4LWEzMmNlMjA2NTJkYlwvZDJ1aXhpOS0wZGY2ZGMxZC04MzczLTQxOWEtYTU5Mi1kYzgyZDYyOGQ4NmYuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.0YzfCBLJZTWaMaOwyT3vwEKj_JM3JkJYNHt3MarRPUE" class = "h_600 " alt="">
 
-    <h1> ${ member.name} </h1>
+    <h1> ${ addmember.name} </h1>
     
-    <h3>${ member.role}  </h3> 
+    <h3>${ addmember.role}  </h3> 
     `;
 
     memberpage.classList.add( `card`, `text-center`, `mt-5`, )
     memeberOnPage.append(memberpage)
     }
-    console.log(team)
     
-})  */
+    
+) 
