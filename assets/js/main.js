a21efa34752b15720e894173286370c0d14c4433
GabriPini <lele19977@gmail.com>
2022-04-07 16:59:57 +0200

    refix h1

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,7 +80,7 @@ addButton.addEventListener('click', function () {
     ` 
     <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f80b6e3-05fc-4af2-a638-a32ce20652db/d2uixi9-0df6dc1d-8373-419a-a592-dc82d628d86f.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmODBiNmUzLTA1ZmMtNGFmMi1hNjM4LWEzMmNlMjA2NTJkYlwvZDJ1aXhpOS0wZGY2ZGMxZC04MzczLTQxOWEtYTU5Mi1kYzgyZDYyOGQ4NmYuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.0YzfCBLJZTWaMaOwyT3vwEKj_JM3JkJYNHt3MarRPUE" class = "h_600 " alt="">
 
-    <h1> ${ addmember.name} </h1>
+    <h2> ${ addmember.name} </h2>
     
     <h3>${ addmember.role}  </h3> 
     `;
